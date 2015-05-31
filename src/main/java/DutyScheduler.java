39cebc99dcd3323d518a3286ccf5e54d89899a5c
desrepair <kl2679@columbia.edu>
2015-05-31 16:26:09 -0400

    Basic UUID & Cookie functionality for persistence.
    A rough draft of how cookies will be assigned and associated with a
    UUID.

--- src/main/java/DutyScheduler.java
+++ src/main/java/DutyScheduler.java
@@ -1,5 +1,8 @@
 
+import java.util.UUID;
+
 import static spark.Spark.*;
+
 import SchedulingHeuristic.*;
 
 public class DutyScheduler {
@@ -8,20 +11,38 @@ public class DutyScheduler {
     public static void main(String[] args) {
          
         get("/", (request, response) -> {
+            //Associate cookie.
+            String uuid = request.cookie("DutyScheduler");
+            if (uuid == null) {
+                uuid = UUID.randomUUID().toString();
+                response.cookie("DutyScheduler", uuid);
+            }
             //Serve the main page.
             return "This is the main page.";
         });
         
-        post("/submitCal", (request, response) -> {
-            //Create Duty Calendar, associate ID.
+        get("/submitCal", (request, response) -> {
+            //Create Duty Calendar
+            //Associate cookie.
+            String uuid = request.cookie("DutyScheduler");
+            if (uuid == null) {
+                uuid = UUID.randomUUID().toString();
+                response.cookie("DutyScheduler", uuid);
+            }
             //Returns a scheduled calendar.
             return "You have submitted a calendar.";
         });
         
         get("/authorize", (request, response) -> {
+            //Check for cookie.
+            String uuid = request.cookie("DutyScheduler");
+            if (uuid == null) {
+                uuid = UUID.randomUUID().toString();
+                response.cookie("DutyScheduler", uuid);
+            }
             //Check for authorization.
             com.google.api.client.auth.oauth2.Credential credential
-                    = GoogleCalendarApiAccess.getStoredCredential("ID"); //TODO: ID system.
+                    = GoogleCalendarApiAccess.getStoredCredential(uuid); //TODO: ID system.
             //If not authorized, redirect to authorization.
             if (credential == null) {
                 String url = GoogleCalendarApiAccess.requestAuthorizationCode();
@@ -38,6 +59,13 @@ public class DutyScheduler {
             String authCode = request.queryString();
             if (authCode == null) {
                 halt(403, "Forbidden");
+                return "";
+            }
+            
+            //Checks for cookie.
+            String uuid = request.cookie("DutyScheduler");
+            if (uuid == null) {
+                halt(403, "Missing required cookie.");
             }
             //Extracts the code.
             String[] parts = authCode.split("=");
@@ -46,7 +74,7 @@ public class DutyScheduler {
                 com.google.api.client.auth.oauth2.Credential credential
                         = GoogleCalendarApiAccess.getTokenCredential(
                         parts[1],
-                        "ID");
+                        uuid); //TODO: Get ID here.
                 //Refreshes client page, asks for calendar submission.
                 //Maybe activate a grayed out "Export Calendar" button.
                 response.redirect("khetthail.me:4567/");
@@ -58,14 +86,19 @@ public class DutyScheduler {
             return "";
         });
         
-        post("/pushToCal", (request, repsonse) -> {
+        get("/pushToCal", (request, repsonse) -> {
+            //Check for cookie.
+            String uuid = request.cookie("DutyScheduler");
+            if (uuid == null) {
+                halt(401, "Missing required cookie");
+            }
             //Check for authentication.
             com.google.api.client.auth.oauth2.Credential credential
-                    = GoogleCalendarApiAccess.getStoredCredential("ID");
+                    = GoogleCalendarApiAccess.getStoredCredential(uuid);
             if (credential == null) {
                 halt(401, "Unauthorized");
             }
-            //Create calendar based on JSON
+            //Create calendar based on JSON.
             //Push calendar to Google.
             return "Your calendar has been exported to your Google Calendar";
         });
