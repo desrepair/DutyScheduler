c303abcddc3a7cec98510eb59d47eb4a95fa668b
desrepair <kl2679@columbia.edu>
2016-02-17 00:07:06 -0800

    Fixed code gen

--- src/main/java/SchedulingHeuristic/DutySchedulerGui.java
+++ src/main/java/SchedulingHeuristic/DutySchedulerGui.java
@@ -570,27 +570,34 @@ public class DutySchedulerGui {
         ScheduleDutyButton.setText("Schedule Duty");
         DutySchedulerGui.add(ScheduleDutyButton, new GridConstraints(3, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         ResultPanel = new JPanel();
-        ResultPanel.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        ResultPanel.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
         ResultPanel.setVisible(true);
         DutySchedulerGui.add(ResultPanel, new GridConstraints(4, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         OutputScrollPane = new JScrollPane();
-        ResultPanel.add(OutputScrollPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        ResultPanel.add(OutputScrollPane, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         OutputTextArea = new JTextArea();
         OutputTextArea.setEditable(false);
         OutputTextArea.setRows(10);
         OutputTextArea.setText("");
         OutputScrollPane.setViewportView(OutputTextArea);
         SubmitToGCalPanel = new JPanel();
-        SubmitToGCalPanel.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
-        ResultPanel.add(SubmitToGCalPanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        SubmitToGCalPanel.setLayout(new GridLayoutManager(2, 3, new Insets(0, 0, 0, 0), -1, -1));
+        ResultPanel.add(SubmitToGCalPanel, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         CalendarNameLabel = new JLabel();
         CalendarNameLabel.setText("Calendar Name");
-        SubmitToGCalPanel.add(CalendarNameLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        SubmitToGCalPanel.add(CalendarNameLabel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         CalendarNameTextField = new JTextField();
-        SubmitToGCalPanel.add(CalendarNameTextField, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        SubmitToGCalPanel.add(CalendarNameTextField, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         SubmitToGCalButton = new JButton();
         SubmitToGCalButton.setText("Export Scheduled Calendar To Google Calendar (Browser Window Will Appear)");
-        SubmitToGCalPanel.add(SubmitToGCalButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        SubmitToGCalPanel.add(SubmitToGCalButton, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        ExistingCalendarRadioButton = new JRadioButton();
+        ExistingCalendarRadioButton.setText("Export to Existing Calendar");
+        SubmitToGCalPanel.add(ExistingCalendarRadioButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        NewCalendarRadioButton = new JRadioButton();
+        NewCalendarRadioButton.setSelected(true);
+        NewCalendarRadioButton.setText("Create New Calendar");
+        SubmitToGCalPanel.add(NewCalendarRadioButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
         DutySchedulerGui.add(spacer2, new GridConstraints(4, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         DutyBlockLengthLabel.setLabelFor(DutyBlockLengthTextField);
@@ -603,6 +610,10 @@ public class DutySchedulerGui {
         SatDayValueLabel.setLabelFor(SatDayValueTextField);
         SunDayValueLabel.setLabelFor(SunDayValueTextField);
         CalendarNameLabel.setLabelFor(DutyBlockLengthTextField);
+        ButtonGroup buttonGroup;
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(ExistingCalendarRadioButton);
+        buttonGroup.add(NewCalendarRadioButton);
     }
 
     /**
